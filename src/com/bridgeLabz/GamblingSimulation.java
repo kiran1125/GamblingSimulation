80f6a168f6f58dfbee567088911c2f5a60736c4b
kiran1125 <makamkiran95@gmail.com>
2021-10-20 08:45:24 +0530

    Started Gambling Simulation

---
+++ src/com/bridgeLabz/GamblingSimulation.java
@@ -0,0 +1,14 @@
+package com.bridgeLabz;
+
+public class GamblingSimulation {
+    static void Gambler(){
+        final int EVERY_DAY_STAKE = 100;
+        final int BET = 1;
+        System.out.println("Every day stake : " + EVERY_DAY_STAKE + " Bet for each game : " + BET);
+    }
+
+    public static void main(String[] args) {
+        Gambler();
+
+    }
+}
