fe9e9a563e719761d60b1df4fde6520daef27d93
kiran1125 <makamkiran95@gmail.com>
2021-10-28 20:46:38 +0530

    Refactored: Assigning values

--- src/com/bridgeLabz/GamblingSimulation.java
+++ src/com/bridgeLabz/GamblingSimulation.java
@@ -1,14 +1,14 @@
 package com.bridgeLabz;
 
 public class GamblingSimulation {
-    static void Gambler(){
-        final int EVERY_DAY_STAKE = 100;
+    static void gambler(){
+        int stake = 100;
         final int BET = 1;
-        System.out.println("Every day stake : " + EVERY_DAY_STAKE + " Bet for each game : " + BET);
+        System.out.println("Every day stake : " + stake + " Bet for each game : " + BET);
     }
 
     public static void main(String[] args) {
-        Gambler();
+        gambler();
 
     }
 }
